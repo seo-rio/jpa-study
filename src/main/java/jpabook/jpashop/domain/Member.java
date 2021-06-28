71ef570b5e0f9521ef6c6968f290ea383d10d2eb
seo-rio <scs9310@gmail.com>
2021-06-28 17:30:28 +0900

    JPA 개발전 Entity 설정

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long Id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") // 1:다 관계 명시, mappedBy order table에 있는 member와 연결되있다. readonly!
+    private List<Order> orders = new ArrayList<>();
+}
