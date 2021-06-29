b59e64bef7854bf7e5ac93d15caec4bde39a2e64
seo-rio <scs9310@gmail.com>
2021-06-29 18:09:59 +0900

    Embeddable 클래스에 대한 생성자 선언 작업

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,7 +1,6 @@
 package jpabook.jpashop.domain;
 
 import lombok.Getter;
-
 import javax.persistence.Embeddable;
 
 @Embeddable
@@ -9,8 +8,16 @@ import javax.persistence.Embeddable;
 public class Address {
 
     private String city;
-
     private String street;
-
     private String zipcode;
+
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+
+    protected Address() {
+    }
 }
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -2,7 +2,6 @@ package jpabook.jpashop.domain;
 
 import lombok.Getter;
 import lombok.Setter;
-
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
