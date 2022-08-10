fdcf4e324bce7621a6e9659f136072f0cfbf756c
seo-rio <scs9310@gmail.com>
2022-08-11 00:04:48 +0900

    # 수정 완료 - Version.2 주문 조회 API 개발 주석 추가

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -48,10 +48,10 @@ public class OrderSimpleApiController {
 
         public SimpleOrderDto(Order order) {
             orderId = order.getId();
-            name = order.getMember().getName();
+            name = order.getMember().getName(); // Lazy 초기화
             orderDate = order.getOrderDate();
             orderStatus = order.getStatus();
-            address = order.getDelivery().getAddress();
+            address = order.getDelivery().getAddress(); // Lazy 초기화
         }
     }
 }
